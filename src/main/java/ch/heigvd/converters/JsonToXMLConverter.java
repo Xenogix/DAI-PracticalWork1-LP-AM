b35726bfcb473d6e80e8bbfffd210ccef7bed9c8
Lionel Pollien <lionel.pollien@heig-vd.ch>
2023-10-07 17:34:46 +0200

    Converters between Xml and json without parameters

--- src/main/java/ch/heigvd/converters/JsonToXMLConverter.java
+++ src/main/java/ch/heigvd/converters/JsonToXMLConverter.java
@@ -2,12 +2,34 @@ package ch.heigvd.converters;
 
 import ch.heigvd.abstractions.IConverterBase;
 import ch.heigvd.parameters.JsonToXMLParameters;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 
+import java.io.File;
 import java.io.IOException;
 
 public class JsonToXMLConverter implements IConverterBase<JsonToXMLParameters> {
     @Override
     public boolean Convert(String inputFile, String outputFile, JsonToXMLParameters parameters) throws IOException {
-        return false;
+        // Create an instance of ObjectMapper for JSON
+        ObjectMapper jsonMapper = new ObjectMapper();
+
+        // Read JSON from a file and convert it to JsonNode
+        JsonNode jsonTree = jsonMapper.readTree(new File(inputFile));
+
+        // Create an instance of XmlMapper for XML and configure indentation and info
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+        xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_1_1, true);
+
+        // Convert to Xml file
+        xmlMapper.writeValue(new File(outputFile), jsonTree);
+
+        return true;
     }
 }
